de05d355ebe897e1a86206e01c7e6d0cb3a3e6a9
Macarena Pazos <macarena.pazos@mi.unc.edu.ar>
2020-09-25 22:09:43 -0300

    Updates

---
+++ precache-manifest.784eab31f7b4d0263c753b47432a3478.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb0b9f96ffbc0b4305c17f0d5aed8ed2",
+    "url": "/robotfriend/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robotfriend/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "c28618d736e59d285326",
+    "url": "/robotfriend/static/css/main.b61b80bb.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robotfriend/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robotfriend/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c28618d736e59d285326",
+    "url": "/robotfriend/static/js/main.42a9313b.chunk.js"
+  },
+  {
+    "revision": "6ac265c37d72569cb05e",
+    "url": "/robotfriend/static/js/runtime-main.07c1a8fb.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
